242b0262e3c9f93327f08d4310369c2eeb0f8353
HeX The Cat <60222984+HeX-The-Cat@users.noreply.github.com>
2024-04-19 16:06:24 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import Picture from "./Picture"
+
+const App = () => {
+  const [text, setText] = useState("")
+
+  const handleTextChange = (event) => {
+    setText(event.target.value)
+  }
+
+  return (
+    <>
+      <div className=" w-screen h-screen bg-gradient-to-r from-slate-950 to-cyan-500">
+        <div className=" flex flex-col text-center">
+          <Picture text={text} />
+
+          <br/>
+
+          <label className="">
+            <input placeholder="Cat wants to say something!" className=" text-center border-solid border-2 border-black rounded-lg w-96" type="text" value={text} onChange={handleTextChange} />
+          </label>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Picture.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types"
+import { useState } from "react"
+import axios from "axios"
+
+const Picture = ({text}) => {
+  const [catPic, setCatPic] = useState("https://cataas.com/cat")
+
+  const newCat = async () => {
+    if(text.trim() === "") {
+      setCatPic("")
+      await axios.get("https://cataas.com/cat")
+        .then((response) => setCatPic(response.config.url))
+    } else {
+      setCatPic("")
+      await axios.get(`https://cataas.com/cat/says/${text.trim()}`)
+        .then((response) => setCatPic(response.config.url))
+    }
+  }
+
+  return (
+    <div className=" w-full flex flex-col justify-center items-center mt-5">
+      <div className=" mb-4 w-[750px] h-[750px] flex justify-center items-center ">
+        <img className=" max-w-[750px] max-h-[750px] rounded-xl" src={catPic} />
+      </div>
+
+      <button className=" p-5 border-solid border-2 rounded-lg font-bold text-xl text-slate-50" onClick={newCat}>Gib New Cat!</button>
+    </div>
+  )
+}
+
+export default Picture
+
+Picture.propTypes = {
+  text: PropTypes.string
+}
